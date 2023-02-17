4ba1af7b58810e3d26c5e8f3b65b4839ff339002
ArthiMega <arthi.meganathan@aspiresys.com>
2023-02-17 11:06:02 +0530

    fs

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var calc = require('./calc')
-
+var fs = require('fs');
+fs.readFile('calc.js','utf8',function(err,data){
+    console.log(data)
+})
+fs.writeFile('newFile','console .log(arthi)' , function(err){
+    console.log('data saved');
+})
+fs.appendFile('newFile','arthi="developer"',function(err){
+    console.log('data saved')
+})
+fs.unlink('newFile',function(err){
+    console.log("Deleted")
+})
 result = calc.add(2,3)
 
-console.log("result of add is",result)
+console.log("result of add is",result)       
