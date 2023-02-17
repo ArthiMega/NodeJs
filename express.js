dd2c89d6d767ee17d35a6586e774ee56bbc21256
ArthiMega <arthi.meganathan@aspiresys.com>
2023-02-17 17:44:13 +0530

    http

--- express.js
+++ express.js
@@ -7,7 +7,7 @@ app.get('/',function(req,res){
 })
 app.get('/arthi',function(req,res){
     const id = req.query.id
-    res.send('Hi Arthi Welcome to quary statement ' + id)
+    res.send('Hi Arthi Welcome to quary statement ' + id )
 })
 app.get('/arthi/:id', function(req, res){
     const id = req.params.id
