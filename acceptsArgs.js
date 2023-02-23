e5a9eaf7c1196345ae70b018612c85b640063151
ArthiMega <arthi.meganathan@aspiresys.com>
2023-02-23 15:23:45 +0530

    node

--- acceptsArgs.js
+++ acceptsArgs.js
@@ -1,5 +1,8 @@
 // process.argv.forEach((val,index)=>{
 //     console.log(`${index}:${val}`)
+
+// const { default: chalk } = require('chalk');
+
 // })
 const args = require('minimist')( process.argv.slice(2));
 // console.log(args['name']); //  node acceptsArgs.js --name=arthi
@@ -47,3 +50,62 @@ oranges.forEach(fruit=>{
 const function1 = ()=>console.trace()
 const function2 = ()=> function1()
 function2()
+
+// ================ CALCULATE THE TIME SPENT==================
+
+const myFunction = ()=>{
+    for(let i= 0; i < 100; i++){
+        console.log(i)
+    }
+}
+const measureMyFunction = ()=>{
+    console.time('myFunction()')
+    myFunction()
+    console.timeEnd('myFunction()')
+}
+measureMyFunction()
+
+// ====================STDOUT and STDERR  ===========================
+
+console.error("Your ego");
+console.log('\x1b[33m%s\x1b[0m', 'hi!')
+
+//  ==============COLOR THE output==================================
+
+// const chalk = require('chalk')
+// console.log(chalk.yellow('hi!'))
+// ==================PROGRESS BAR =========================
+const ProgressBar = require('progress')
+
+const bar = new ProgressBar(':bar', { total: 10})
+const timer = setInterval(() => {
+  bar.tick()
+  if (bar.complete) {
+    clearInterval(timer)
+  }
+}, 100)
+
+// ?=============== ACCEPTS INPUTS FROM COMMEND LINE   (READ LINE) ===================
+const  readline = require('readline').createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+readline.question(`What is your name?`,name=>{
+    console.log(`Hi di pattu thangam chellam vairom etc,. ${name}!`)
+    readline.close()
+})
+
+// ============================= ACCEPTS INPUTS FROM COMMEND LINE  (INQUIRER) =======================
+// import inquirer from 'inquirer.js';
+
+// var questions = [
+//   {
+//     type: 'input',
+//     name: 'name',
+//     message: "What's your name?"
+//   }
+// ]
+
+// inquirer.prompt(questions).then(answers => {
+//   console.log(`Hi ${answers['name']}!`)
+// })
--- calc.js
+++ calc.js
@@ -4,3 +4,8 @@ exports.add =  function add(a,b){//(3)
 
 // module.exports.add = add (1)
 // exports.add = add (2)
+cars = {
+    brand:'Ford',
+    cast:1000000
+}
+module.exports.cars = cars
