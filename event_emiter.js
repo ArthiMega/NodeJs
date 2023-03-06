758ac30aeff91b23c45372a8abf8f2b8e4bc743b
ArthiMega <arthi.meganathan@aspiresys.com>
2023-03-06 18:04:49 +0530

    express

--- event_emiter.js
+++ event_emiter.js
@@ -5,3 +5,8 @@ eventEmiter.on('message',function(){
     console.log("Hi")
 })
 eventEmiter.emit('message')
+eventEmiter.setMaxListeners(60)
+console.log(eventEmiter.eventNames())
+console.log(eventEmiter.getMaxListeners())
+console.log(eventEmiter.listenerCount('message'))
+console.log(eventEmiter.listeners('message'))
--- express.js
+++ express.js
@@ -1,18 +1,41 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.get('/',function(req,res){
-    res.send('Hello World!')
+// app.get('/',function(req,res){
+//     res.send('Hello World!')
+// })
+// app.get('/arthi',function(req,res){
+//     const id = req.query.id
+//     res.send('Hi Arthi Welcome to quary statement ' + id )
+// })
+// app.get('/arthi/:id', function(req, res){
+//     const id = req.params.id
+//     res.send(' Wow Arthi! ' + id)
+// })
+// app.post('/user',(req,res)=>{
+//     res.send('Got a post request......')
+// })
+// app.listen(9000, function(req,res){
+//     console.log('Running.... ')
+// });
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get('/',(req,res)=>{
+    res.send("Hello World!")
 })
-app.get('/arthi',function(req,res){
-    const id = req.query.id
-    res.send('Hi Arthi Welcome to quary statement ' + id )
+
+app.post('/',(req,res)=>{
+    let data = req.body;
+    res.send('Data Recieved: '+JSON.stringify(data))
 })
-app.get('/arthi/:id', function(req, res){
-    const id = req.params.id
-    res.send(' Wow Arthi! ' + id)
+
+app.listen(5000,()=>{
+    console.log('Example app listening on port 5000')
 })
-app.listen(9000, function(req,res){
-    console.log('Running.... ')
-});
